a948c4270ce9914091dc3d853c340d05aec7c6f9
gregorymarchal <114147362+gregorymarchal@users.noreply.github.com>
2024-06-14 16:39:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -22,4 +22,4 @@ async def predict(input: TextInput):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=5000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
