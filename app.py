d81cade2e35bbb34d6fc90bbae89331bd3a1f7b3
gregorymarchal <114147362+gregorymarchal@users.noreply.github.com>
2024-06-14 20:47:11 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
-from fastapi import FastAPI
+from flask import Flask, jsonify
 
-app = FastAPI()
+app = Flask(__name__)
 
-@app.get("/")
+@app.route("/")
 def read_root():
-    return {"Hello": "World"}
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    return jsonify({"Hello": "World"})
