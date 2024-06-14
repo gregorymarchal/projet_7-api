9b20d0e0fb68c32543c7bff8f5eed1477c1587eb
gregorymarchal <114147362+gregorymarchal@users.noreply.github.com>
2024-06-14 19:15:33 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,10 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
-from transformers import AutoTokenizer, TFAutoModelForSequenceClassification
-import tensorflow as tf
 
 app = FastAPI()
 
-model_name = "gregorymarchal/projet-7-trained-distilbert"
-model = TFAutoModelForSequenceClassification.from_pretrained(model_name)
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-
-class TextInput(BaseModel):
-    text: str
-
-@app.post("/predict")
-async def predict(input: TextInput):
-    inputs = tokenizer(input.text, return_tensors="tf")
-    outputs = model(inputs.data)
-    logits = outputs.logits
-    predicted_class_id = tf.argmax(logits, axis=1).numpy()[0]
-    return {"predicted_class_id": int(predicted_class_id)}
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
 
 if __name__ == "__main__":
     import uvicorn
