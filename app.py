adc624f19d0047cb7cd6065fa148ecc37909c2b0
gregorymarchal <114147362+gregorymarchal@users.noreply.github.com>
2024-07-07 21:07:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # Import necessary modules from Flask and TensorFlow, and Hugging Face's Transformers library
 from flask import Flask, request, jsonify
+import os
 import tensorflow as tf
 from transformers import TFAutoModelForSequenceClassification, AutoTokenizer
 import logging
@@ -18,7 +19,7 @@ model = TFAutoModelForSequenceClassification.from_pretrained(model_name)
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 
 # Set up logging to Azure Application Insights
-connection_string = "InstrumentationKey=55f8c386-676e-4155-b920-c470270eb854;IngestionEndpoint=https://francecentral-1.in.applicationinsights.azure.com/;LiveEndpoint=https://francecentral.livediagnostics.monitor.azure.com/;ApplicationId=499673b2-6e2a-4c76-a53c-2ad71070f331"
+connection_string = os.getenv('CONNECTION_STRING')
 logger = logging.getLogger(__name__)
 logger.addHandler(AzureLogHandler(connection_string=connection_string))
 
